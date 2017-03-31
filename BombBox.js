3f6872a1b85ab34d6af616e76f13943efd37bc8a
kpsurreal <648587198@qq.com>
2017-03-31 16:13:33 +0800

    修改了useInfo by lgy

--- BombBox.js
+++ BombBox.js
@@ -9,7 +9,7 @@ var mysql = require('mysql');
 //连接mysql所需的用户名和密码
 var server = mysql.createConnection({
   user:'root',
-  database:'hq',
+  database:'shijianfeishi',
   password:''
 });
 //加载 body-parser 中间件（第三方模块）
